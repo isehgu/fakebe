f8671860be31d6de9b46000af1fbfbec528d7407
Han Xiao Gu <hanguu@us08mac00082.fios-router.home>
2017-09-12 14:00:28 -0400

    working version faking graphite

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const _ = require('lodash');
+
+const port = process.env.PORT || 1337;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(port, () => {
+    console.log(`Webserver is listening on ${port}`);
+});
+
+app.all('/metrics/find', (req, res) => {
+    console.log('Got metric request')
+    setCORSHeaders(res);
+    var response = [
+        { leaf: 1, text: '1st metric'},
+        { leaf: 2, text: '2nd metric'},
+        { leaf: 3, text: '3rd metric'},
+    ]
+    res.json(response);
+})
+
+app.post('/render', (req, res) => {
+    setCORSHeaders(res);
+    console.log("Got render request");
+    console.log(req.body);
+    var re = /-([0-9])[m]/i;
+    var now = Math.floor(Date.now()/1000);
+    // { target: 'group(1st metric, group(1st metric, #A, #B), #B)',
+    //   from: '-1s',
+    //   until: 'now',
+    //   format: 'json',
+    //   maxDataPoints: '1321' }
+    const from = now - (req.body.from.match(re)[1] * 60);
+    console.log(from);
+    const diff = now - from
+    // const interval = Math.floor(diff/10000);
+    console.log("diff ", diff);
+    // console.log("interval ", interval);
+    var data = _.range(from, now).map((time) => {
+        return [Math.floor(Math.random()*10), time];
+    })
+    // console.log(data);
+    res.json([{
+        target: req.body.target,
+        datapoints: data
+        // datapoints: [[Math.floor(Math.random() * 100), now-1]]
+    }])
+})
+
+
+function setCORSHeaders(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "POST");
+  // res.setHeader("Access-Control-Allow-Headers", "accept, content-type");
+}
