d3541bfcc9260470af45cd8a1c516ff72a34c65a
Han Xiao Gu <hanguu@us08mac00082.fios-router.home>
2017-09-15 16:45:03 -0400

    modified for custom plugin

--- server.js
+++ server.js
@@ -17,13 +17,22 @@ app.use((req, res, next) => {
     next();
 })
 
+app.get('/', (req, res) => {
+    res.send('Looks good');
+})
+
 app.all('/metrics/find', (req, res) => {
     console.log('Got metric request')
-    var response = [
-        { leaf: 1, text: '1st metric'},
-        { leaf: 2, text: '2nd metric'},
-        { leaf: 3, text: '3rd metric'},
-    ]
+    console.log(req);
+    // For fake graphite
+    // var response = [
+    //     { leaf: 1, text: '1st metric'},
+    //     { leaf: 2, text: '2nd metric'},
+    //     { leaf: 3, text: '3rd metric'},
+    // ]
+
+    // For custom plugin
+    var response = ['1st metric', '2nd metric', '3rd metric'];
     res.json(response);
 })
 
@@ -38,28 +47,56 @@ app.post('/render', (req, res) => {
     //   until: 'now',
     //   format: 'json',
     //   maxDataPoints: '1321' }
-    const from = now-(req.body.from.match(re)[1] * 60); // From is now - the specified min
+
+    // graphite
+    // const from = now-(req.body.from.match(re)[1] * 60); // From is now - the specified min
+
+    // Custom plugin
+    const from = now - (req.body.rangeRaw.from.match(re)[1] * 60);
     console.log(from);
     const diff = now - from
     // const interval = Math.floor(diff/10000);
     console.log("diff ", diff);
     // console.log("interval ", interval);
 
+    // Graphite
     // Generating data per second
-    var data = _.range(from, now).map((time) => {
-        return [Math.floor(Math.random()*10), time];
-    })
+    // var data = _.range(from, now).map((time) => {
+    //     return [Math.floor(Math.random()*10), time*1000]; // Restoring time back to milliseconds
+    // })
     // console.log(data);
-    res.json([{
-        target: req.body.target,
-        datapoints: data
-        // datapoints: [[Math.floor(Math.random() * 100), now-1]]
-    }])
+
+
+
+    // res.json([{
+    //     target: req.body.target,
+    //     datapoints: data
+    //     // datapoints: [[Math.floor(Math.random() * 100), now-1]]
+    // }])
+
+    // Custom plugin
+    let resp = [],
+        respObj = {};
+    // TargetObj is -- targets: [ { target: '1st metric', refId: 'A', type: 'timeserie' } ],
+    // resp is [ targetObj, targetObj, targetObj...]
+    console.log(Array.isArray(req.body.targets))
+    resp = req.body.targets.map(function(targetObj, idx){
+        respObj = {},
+        respObj.target = targetObj.target;
+        respObj.datapoints = _.range(from, now).map((time) => {
+
+            // idx+1 is really for the first element, where index is actually 0, which renders all datapoints 0.
+            return [Math.floor(Math.random()*10*(idx+1)), time*1000]; // Restoring time back to milliseconds
+        })
+        return respObj;
+    })
+
+    res.json(resp);
 })
 
 
 function setCORSHeaders(res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "POST");
-  // res.setHeader("Access-Control-Allow-Headers", "accept, content-type");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "accept, content-type");
 }
